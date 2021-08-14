2f0c82ee679b5587454ff4fdfc068bea5ea82f0d
AdrielPresley <adriel_tel11@hotmail.com>
2021-08-13 21:17:53 -0300

    Initial commit

---
+++ src/main/java/com/dio/controller/JornadaTrabalhoController.java
@@ -0,0 +1,56 @@
+package com.dio.controller;
+
+import com.dio.model.JornadaTrabalho;
+import com.dio.service.JornadaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/jornada")
+public class JornadaTrabalhoController {
+    final
+    JornadaService jornadaService;
+
+    public JornadaTrabalhoController(JornadaService jornadaService) {
+        this.jornadaService = jornadaService;
+    }
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> getJornadaList(){
+        return jornadaService.findAll();
+
+    }
+
+    @GetMapping("/{idJornada}")
+    public ResponseEntity<JornadaTrabalho>  getJornadaByID(@PathVariable("idJornada") Long idJornada) {
+        return  ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Not found!")));
+
+    }
+
+    @PutMapping
+    public JornadaTrabalho updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.updateJornada(jornadaTrabalho);
+    }
+
+    @DeleteMapping("/{idJornada}")
+    public ResponseEntity<JornadaTrabalho> deleteByID(@PathVariable("idJornada") Long idJornada) {
+       try {
+           jornadaService.deleteJornada(idJornada);
+       }catch (Exception e){
+           System.out.println(e.getMessage());
+       }
+        return null;
+
+    }
+
+
+
+}
